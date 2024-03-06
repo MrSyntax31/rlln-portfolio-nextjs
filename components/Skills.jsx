b420d30383e5e920e06005c18edb3fcfadedd1be
Arllan <60104295+MrSyntax31@users.noreply.github.com>
2024-03-06 17:52:06 +0800

    Update Skills.jsx
    Fix isses

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -100,7 +100,7 @@ const Skills = () => {
             </div>
           </div>
 
-          <!--Skillsv2-->
+      
                     <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                       <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
