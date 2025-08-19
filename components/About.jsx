3737a3f8ff0ce7ecfd9196d9f230506a1365e41f
Arllan <60104295+MrSyntax31@users.noreply.github.com>
2025-08-19 12:27:39 +0400

    Update About.jsx
    Test response to vercel update live production.

--- components/About.jsx
+++ components/About.jsx
@@ -13,14 +13,7 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            Test
           </p>
           <p className='py-2 text-gray-600'>
             I started web developement in 2013 managing multiple e-commerce
